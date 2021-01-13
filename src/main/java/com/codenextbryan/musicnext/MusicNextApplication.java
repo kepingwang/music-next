97085d8dbe3c02f858265e25b086b180d4f61b47
Keping <kepingwang@yahoo.com>
2021-01-13 01:20:35 -0500

    initial commit from sprint initializer

---
+++ src/main/java/com/codenextbryan/musicnext/MusicNextApplication.java
@@ -0,0 +1,13 @@
+package com.codenextbryan.musicnext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicNextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicNextApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codenextbryan/musicnext/MusicNextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codenextbryan.musicnext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicNextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
