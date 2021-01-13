58edc1fb4bb40aab99d86f89d60b5ae0ca539e6f
Keping <kepingwang@yahoo.com>
2021-01-13 01:27:31 -0500

    create controller for root / path

---
+++ src/main/java/com/codenextbryan/musicnext/HelloController.java
@@ -0,0 +1,16 @@
+package com.codenextbryan.musicnext;
+
+// Following https://spring.io/guides/gs/spring-boot/
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
